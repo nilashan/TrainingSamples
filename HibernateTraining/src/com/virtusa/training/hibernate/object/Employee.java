4937bb37d345d2fe449c275dea65b8787a926fd5
nilashan <nilashan@live.com>
2014-11-17 14:53:47 +0530

    eee

--- HibernateTraining/src/com/virtusa/training/hibernate/object/Employee.java
+++ HibernateTraining/src/com/virtusa/training/hibernate/object/Employee.java
@@ -5,12 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 public class Employee {
 
 	private int empId;
