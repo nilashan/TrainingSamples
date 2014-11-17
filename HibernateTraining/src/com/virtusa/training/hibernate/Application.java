7f106b0b16b80d6bfc6fc0326bc30fb82f28111a
nilashan <nilashan@live.com>
2014-11-17 16:46:28 +0530

    final commit

--- HibernateTraining/src/com/virtusa/training/hibernate/Application.java
+++ HibernateTraining/src/com/virtusa/training/hibernate/Application.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.hibernate.Session;
 
 import com.virtusa.training.hibernate.object.Employee;
-import com.virtusa.training.hibernate.object.AllocationData;
 import com.virtusa.training.hibernate.object.AllocationHistory;
 import com.virtusa.training.hibernate.util.HibernateUtilities;
 
@@ -21,13 +20,13 @@ public class Application {
 		Session session = HibernateUtilities.getSessionFactory().openSession();
 		session.beginTransaction();
 		Employee employee = new Employee();
-		employee.addAllocationHistory(new AllocationHistory(new Date(), "wso2"));
+		employee.addAllocationHistory(new AllocationHistory(new Date(), "Hibernatettt"));
 
-		employee.setName("Krishantha");
+		employee.setName("Nilashan");
 		employee.setAge(30);
-		employee.getAllocationData().setCity("Kadawatha");
-		employee.getAllocationData().setMobile("071456");
-		employee.addAllocationHistory(new AllocationHistory(new Date(), "ICTA"));
+		employee.getAllocationData().setCity("Colombo");
+		employee.getAllocationData().setMobile("0771236813");
+		employee.addAllocationHistory(new AllocationHistory(new Date(), "Virtusa"));
 		session.save(employee);
 		session.getTransaction().commit();
 		session.close();
